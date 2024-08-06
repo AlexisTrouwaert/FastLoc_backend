01413a5b97cf7225f179b1fedd5a80e4b181486a
AlexisTrouwaert <alexis.trouwaert@gmail.com>
2024-08-06 16:24:35 +0200

    t

--- models/users.js
+++ models/users.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 
 const articlesSchema = mongoose.Schema({
+    urlPhoto : String,
     etat: String, 
     price: Number, 
     locaduree: Date, 
--- routes/tools.js
+++ routes/tools.js
@@ -2,5 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Tools = require('../models/tools')
 
+router.post('/addTool', (req, res) => {
+    Tools.findOne({})
+})
 
 module.exports = router;
