1514540b8ab4e97ba74c8f612298247d63d191cd
AlexisTrouwaert <alexis.trouwaert@gmail.com>
2024-08-06 16:27:57 +0200

    t

--- routes/tools.js
+++ routes/tools.js
@@ -3,7 +3,22 @@ const router = express.Router();
 const Tools = require('../models/tools')
 
 router.post('/addTool', (req, res) => {
-    Tools.findOne({})
+    Tools.findOne({brand : req.body.brand, model : req.body.model, categorie : req.body.categorie})
+    .then(data => {
+        if(data === null){
+            const newTool = new Tools({
+                categorie : req.body.categorie,
+                brand : req.body.brand,
+                model : req.body.model
+            })
+            newTool.save()
+            .then(() => {
+                res.json({result : true, newToolInfos : newTool})
+            })
+        } else {
+            res.json({result : true, data: data})
+        }
+    })
 })
 
 module.exports = router;
