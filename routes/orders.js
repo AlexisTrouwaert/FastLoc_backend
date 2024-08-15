5da92ce0ac345b9f93aaca290e4d953c23c69625
AlexisTrouwaert <alexis.trouwaert@gmail.com>
2024-08-15 09:49:25 +0200

    t

--- routes/orders.js
+++ routes/orders.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Users = require('../models/users')
 const Orders = require('../models/orders')
-const Tools = require('../models/tools')
+const Tools = require('../models/tools');
+const { model } = require('mongoose');
 
 router.get('/:username/:token', (req, res) => {
     Users.find({username : req.params.username, token : req.params.token})
@@ -10,6 +11,7 @@ router.get('/:username/:token', (req, res) => {
         if(data){
             Orders.find({locUserId : data[0]._id})
             .then(data2 => {
+                console.log(data2.Orders[0])
                 res.json({data : data2})
             })
         }
